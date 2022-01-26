881dbda96ad5a86afac414d06d0f7a065b429647
AdolfoSaladoLopez <93923566+AdolfoSaladoLopez@users.noreply.github.com>
2022-01-26 09:50:13 +0100

    Update AsignarEquipoTest

--- gestion_equipo/src/AsignarEquipoTest.java
+++ gestion_equipo/src/AsignarEquipoTest.java
@@ -19,18 +19,21 @@ class AsignarEquipoTest {
         asignarEquipo.setJugador(null);
     }
 
-    /* ASIGNAR JUGADOR EXISTENTE CON TODOS LOS CAMPOS V¡LIDOS. Se asigna*/
+    /* ASIGNAR JUGADOR EXISTENTE CON TODOS LOS CAMPOS V√ÅLIDOS. Se asigna*/
     @Test
     void testSetJugadorExistenteCamposValidos(){
         AsignarEquipo asignarEquipo = new AsignarEquipo();
+        
         Jugador jugador = new Jugador();
         String nombre = "thor";
         jugador.setNombreJugador(nombre);
         int edad = 22;
         jugador.setEdad(edad);
-        String idioma = "EspaÒol";
+        String idioma = "Espa√±ol";
         jugador.setIdioma(idioma);
+        
         asignarEquipo.setJugador(jugador);
+        
         Jugador jugadorGuardado = asignarEquipo.getJugador();
         assertNotNull(jugadorGuardado);
         assertEquals(nombre, jugadorGuardado.getNombreJugador());
@@ -38,83 +41,108 @@ class AsignarEquipoTest {
         assertEquals(idioma, jugadorGuardado.getIdioma());
     }
 
-    /* Asignar jugador que exista, pero su nombre sea inv·lido */
+    /* Asignar jugador que exista, pero su nombre sea inv√°lido */
     @Test
     void testSetJugadorExistenteNombreInvalido(){
         AsignarEquipo asignarEquipo = new AsignarEquipo();
         Jugador jugador = new Jugador();
+        
         String nombre = "or";
         jugador.setNombreJugador(nombre);
+        
         int edad = 22;
         jugador.setEdad(edad);
-        String idioma = "EspaÒol";
+        
+        String idioma = "Espa√±ol";
         jugador.setIdioma(idioma);
+        
         asignarEquipo.setJugador(null);
     }
 
-    /* Asignar jugador que exista, pero no tenga nombre (es decir, habÈis creado el jugador pero no habÈis puesto el nombre con setnombre) */
+    /* Asignar jugador que exista, pero no tenga nombre (es decir, hab√©is creado el jugador pero no hab√©is puesto el nombre con setnombre) */
     @Test
     void testJugadorExistenteSinNombre(){
         AsignarEquipo asignarEquipo = new AsignarEquipo();
         Jugador jugador = new Jugador();
+        
+        String nombre = null;
+        
         int edad = 20;
         jugador.setEdad(edad);
-        String idioma = "InglÈs";
+        
+        String idioma = "Ingl√©s";
         jugador.setIdioma(idioma);
+        
         asignarEquipo.setJugador(null);
     }
 
-    /* Asignar jugador que exista, pero su edad sea inv·lida (podeis poner cualquier edad no v·lida, porque ya se prueba todas las combinaciones posibles en el anterior */
+    /* Asignar jugador que exista, pero su edad sea inv√°lida (podeis poner cualquier edad no v√°lida, porque ya se prueba todas las combinaciones posibles en el anterior */
     @Test
     void testJugadorSinNombre(){
         AsignarEquipo asignarEquipo = new AsignarEquipo();
         Jugador jugador = new Jugador();
+        
         String nombre = "Perro";
         jugador.setNombreJugador(nombre);
-        int edad = 0;
+        
+        Integer edad = null;
         jugador.setEdad(edad);
-        String idioma = "InglÈs";
+       
+        String idioma = "Ingl√©s";
         jugador.setIdioma(idioma);
+        
         asignarEquipo.setJugador(null);
     }
 
-    /* Asignar jugador que exista, pero no tenga edad(es decir, habÈis creado el jugador pero no habÈis puesto el edad con setEdad) */
+    /* Asignar jugador que exista, pero no tenga edad(es decir, hab√©is creado el jugador pero no hab√©is puesto el edad con setEdad) */
     @Test
     void testJugadorExistenteSinEdad(){
         AsignarEquipo asignarEquipo = new AsignarEquipo();
         Jugador jugador = new Jugador();
+        
         String nombre = "Perro";
-        Integer edad = null;
         jugador.setNombreJugador(nombre);
-        jugador.setEdad(edad);
-        String idioma = "InglÈs";
+        
+        Integer edad = null;
+        
+        String idioma = "Ingl√©s";
         jugador.setIdioma(idioma);
+        
         asignarEquipo.setJugador(null);
     }
 
-    /* Asignar jugador que exista, pero su idioma sea inv·lido (podeis poner cualquier idioma no v·lido, porque ya se prueba todas las combinaciones posibles en el anterior) */
+    /* Asignar jugador que exista, pero su idioma sea inv√°lido (podeis poner cualquier idioma no v√°lido, porque ya se prueba todas las combinaciones posibles en el anterior) */
     @Test
     void testJugadorExistenteIdiomaInvalido(){
         AsignarEquipo asignarEquipo = new AsignarEquipo();
         Jugador jugador = new Jugador();
-        String nombre = "or";
+        
+        String nombre = "Thor";
         jugador.setNombreJugador(nombre);
+        
         int edad = 22;
         jugador.setEdad(edad);
-        String idioma = "JaponÈs";
+        
+        String idioma = "Japon√©s";
         jugador.setIdioma(idioma);
+        
         asignarEquipo.setJugador(null);
     }
 
-    /* Asignar jugador que exista, pero no tenga idioma(es decir, habÈis creado el jugador pero no habÈis puesto el idioma con setIdioma) */
+    /* Asignar jugador que exista, pero no tenga idioma(es decir, hab√©is creado el jugador pero no hab√©is puesto el idioma con setIdioma) */
     @Test
     void testJugadorExistenteSinIdioma(){
         AsignarEquipo asignarEquipo = new AsignarEquipo();
         Jugador jugador = new Jugador();
+        
         String nombre = "thor";
         jugador.setNombreJugador(nombre);
+        
         int edad = 22;
         jugador.setEdad(edad);
+        
+        String idioma = null;
+        
         asignarEquipo.setJugador(null);
     }
 
@@ -131,7 +159,7 @@ class AsignarEquipoTest {
         assertNull(asignarEquipo.getClass());
     }
 
-    //Asignar equipo que exista y tenga todos sus campos v·lidos
+    //Asignar equipo que exista y tenga todos sus campos v√°lidos
     @Test
     void equipoCorrecto() {
         String nombreEquipo = "Tigretones Malignos";
@@ -148,7 +176,7 @@ class AsignarEquipoTest {
         assertEquals(ranking, asignarEquipo.getEquipo().getRanking());
     }
 
-    //Asignar equipo que exista, pero su nombre sea inv·lido (podÈis poner cualquier nombre no v·lido,
+    //Asignar equipo que exista, pero su nombre sea inv√°lido (pod√©is poner cualquier nombre no v√°lido,
     //porque ya se prueba todas las combinaciones posibles en el anterior)
     @Test
     void equipoNombreInvalido() {
@@ -166,8 +194,8 @@ class AsignarEquipoTest {
         assertEquals(ranking, asignarEquipo.getEquipo().getRanking());
     }
 
-    //Asignar equipo que exista, pero no tenga nombre (es decir, habÈis creado el equipo
-    //pero no habÈis puesto el nombre con setNombre)
+    //Asignar equipo que exista, pero no tenga nombre (es decir, hab√©is creado el equipo
+    //pero no hab√©is puesto el nombre con setNombre)
     @Test
     void equipoNombreNulo() {
         int ranking = 1;
@@ -182,7 +210,7 @@ class AsignarEquipoTest {
         assertEquals(ranking, asignarEquipo.getEquipo().getRanking());
     }
 
-    //Asignar equipo que exista, pero su ranking sea inv·lida (podÈis poner cualquier ranking no v·lido,
+    //Asignar equipo que exista, pero su ranking sea inv√°lida (pod√©is poner cualquier ranking no v√°lido,
     //porque ya se prueba todas las combinaciones posibles en el anterior)
     @Test
     void equipoRankingInvalido() {
@@ -201,10 +229,10 @@ class AsignarEquipoTest {
     }
 
     //Asignar equipo que exista, pero no tenga ranking(es decir,
-    //habÈis creado el equipo pero no habÈis puesto el nombre con setNombre)
+    //hab√©is creado el equipo pero no hab√©is puesto el nombre con setNombre)
 
-    //Problemas aquÌ, porque un entero no puede ser null,
-    //se entiende que null es 0, pero ese es un valor v·lido.
+    //Problemas aqu√≠, porque un entero no puede ser null,
+    //se entiende que null es 0, pero ese es un valor v√°lido.
     @Test
     void equipoRankingNulo() {
         String nombreEquipo = "Los Magufos";
