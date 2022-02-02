a215d50248f907183a7ac1dbe3c354875c3122c1
Nachopper1 <93922627+Nachopper1@users.noreply.github.com>
2022-02-02 10:46:33 +0100

    Añadir tipoJugador a Jugador

--- gestion_equipo/src/Jugador.java
+++ gestion_equipo/src/Jugador.java
@@ -1,63 +1,84 @@
 
 public class Jugador {
-    private String nombre;
-    private int edad;
-    private String idioma;
-
-    public Jugador() {
-    }
-
-    public void setNombreJugador(String nombre) {
-        boolean letrasIngles = true;
-
-        for (int i = 0; i < nombre.length(); i++) {
-            char letra = nombre.charAt(i);
-            if (letra >= 65 && letra <= 90) {
-                //letras mayúsculas
-            } else if (letra >= 97 && letra <= 122) {
-                //letras minúsculas
-            } else if (letra == ' ') {
-                //espacio
-            } else {
-                letrasIngles = false;
-            }
-        }
-
-        if (letrasIngles && nombre.length() >= 4 && nombre.length() <= 20) {
-            this.nombre = nombre;
-        }
-    }
-
-    public void setEdad(int edad) {
-        if (edad >= 18 && edad <= 100) this.edad = edad;
-    }
-
-    public void setIdioma(String idioma) {
-        switch (idioma) {
-            case "inglés":
-            case "español":
-            case "alemán":
-            case "francés":
-                //En caso de que sea alguno de los anteriores, se asigna el idioma,
-                this.idioma = idioma;
-                break;
-            default:
-                //No se asigna idioma.
-                break;
-        }
-
-    }
+	private String nombre;
+	private int edad;
+	private String idioma;
 
 
-    public String getIdioma() {
-        return idioma;
-    }
+	public Jugador() {
+	}
+	public String tipoJugador() {
+		String junior = "Junior";
+		String senior = "Senior";
+		String master = "Master";
 
-    public String getNombreJugador() {
-        return nombre;
-    }
 
-    public int getEdad() {
-        return edad;
-    }
+
+		if(edad>=18 && edad<25) {
+			return junior;	
+		}
+		else if(edad>=25 && edad<=35) {
+			return senior;	
+		}
+		else if(edad>35){
+			return master;	
+		}	
+		return null;
+	}
+
+
+	public void setNombreJugador(String nombre) {
+		boolean letrasIngles = true;
+
+		for (int i = 0; i < nombre.length(); i++) {
+			char letra = nombre.charAt(i);
+			if (letra >= 65 && letra <= 90) {
+				//letras mayúsculas
+			} else if (letra >= 97 && letra <= 122) {
+				//letras minúsculas
+			} else if (letra == ' ') {
+				//espacio
+			} else {
+				letrasIngles = false;
+			}
+		}
+
+		if (letrasIngles && nombre.length() >= 4 && nombre.length() <= 20) {
+			this.nombre = nombre;
+		}
+	}
+
+	public void setEdad(int edad) {
+		if (edad >= 18 && edad <= 100) this.edad = edad;
+		else if(edad==-1) { this.edad=-1;}
+	}
+
+	public void setIdioma(String idioma) {
+		switch (idioma) {
+		case "inglés":
+		case "español":
+		case "alemán":
+		case "francés":
+			//En caso de que sea alguno de los anteriores, se asigna el idioma,
+			this.idioma = idioma;
+			break;
+		default:
+			//No se asigna idioma.
+			break;
+		}
+
+	}
+
+
+	public String getIdioma() {
+		return idioma;
+	}
+
+	public String getNombreJugador() {
+		return nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
 }
