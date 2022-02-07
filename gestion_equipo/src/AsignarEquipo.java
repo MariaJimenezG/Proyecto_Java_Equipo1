ec3c3545022e1aa8850d533231d2e491697180b2
MariaJimenezG <96425567+MariaJimenezG@users.noreply.github.com>
2022-02-07 23:01:04 +0100

    Update AsignarEquipo.java

--- gestion_equipo/src/AsignarEquipo.java
+++ gestion_equipo/src/AsignarEquipo.java
@@ -1,27 +1,42 @@
 
 public class AsignarEquipo {
-      private Equipo equipo;
-      private Jugador jugador;
-      
-      
-      public AsignarEquipo() {
-    	  
-      }
-      
-      public void setEquipo(Equipo equipo) {
-  		//code here
-      }
-      
-      public void setJugador(Jugador jugador) {
-  		//code here
-      }
-      
-      public Equipo getEquipo(){
-          return equipo;
-      }
-      
-      public Jugador getJugador(){
-         return jugador;
-      }
-      
-}
+
+	private Equipo equipo;
+	private Jugador jugador;
+
+
+
+	public AsignarEquipo() {
+
+	}
+
+	public void setEquipo(Equipo equipo) {
+
+		if (equipo != null) {
+			if ( equipo.getNombreEquipo() != null && equipo.getRanking() != 0) {
+
+				this.equipo = equipo;
+
+
+			}  
+		}
+
+
+	}
+
+	public void setJugador(Jugador jugador) {
+
+
+		String idioma = jugador.getIdioma();
+
+		if(jugador != null) {
+			if (jugador.getEdad() != -1 && jugador.getNombreJugador() != null && jugador.getIdioma() != null) {
+
+				this.jugador = jugador;
+
+			}
+
+		}
+
+	}
+
